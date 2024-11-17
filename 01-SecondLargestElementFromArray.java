e78dbe33bd70ed680b9738de643ebbc71ff82913
Tanmay Jadhav <tanmayj4044@gmail.com>
2024-11-17 12:18:21 +0530

    Create 01-SecondLargestElementFromArray.java

---
+++ 01-SecondLargestElementFromArray.java
@@ -0,0 +1,53 @@
+/*Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.*/
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSecondLargest(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
+            }
+        }
+        
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest; 
+    }
+}
