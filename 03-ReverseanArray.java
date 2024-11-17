4e8add6e37dad35e17cb313a67b73148f6e999f6
Tanmay Jadhav <tanmayj4044@gmail.com>
2024-11-17 12:24:22 +0530

    Create 03-ReverseanArray.java

---
+++ 03-ReverseanArray.java
@@ -0,0 +1,44 @@
+/*You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Examples:
+
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+
+            obj.reverseArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            int t = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = t;
+        }
+    }
+}
