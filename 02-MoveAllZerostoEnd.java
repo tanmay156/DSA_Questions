f0a710f3a0250a2b7e5647b786ce72e069eb08bc
Tanmay Jadhav <tanmayj4044@gmail.com>
2024-11-17 12:20:05 +0530

    Create 02-MoveAllZerostoEnd.java

---
+++ 02-MoveAllZerostoEnd.java
@@ -0,0 +1,54 @@
+/*Given an array arr[]. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements. Do the mentioned change in the array in place.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n= arr.length;
+        int count = 0;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] !=0)
+            {
+               arr[count++] = arr[i];
+            }
+
+        }
+        while(count<n)
+        {
+            arr[count++] = 0;
+        }
+
+    }
+}
