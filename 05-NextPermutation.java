034a871a844fe43cd6008b2d52cfe4cc25138972
Tanmay Jadhav <tanmayj4044@gmail.com>
2024-11-19 12:08:05 +0530

    Create 05-NextPermutation.java

---
+++ 05-NextPermutation.java
@@ -0,0 +1,79 @@
+/*  Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. 
+If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
+
+Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
+
+Examples:
+
+Input: arr = [2, 4, 1, 7, 5, 0]
+Output: [2, 4, 5, 0, 1, 7]
+Explanation: The next permutation of the given array is {2, 4, 5, 0, 1, 7}.*/
+
+
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    void nextPermutation(int[] arr) {
+        int n = arr.length;
+        int i = n - 2, j = n - 1;
+
+        while (i >= 0 && arr[i] >= arr[i + 1])
+            i--;
+
+        if (i >= 0) {
+
+            while (arr[j] <= arr[i])
+                j--;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        reverse(arr, i + 1, n - 1);
+    }
+
+    void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine()); // Number of test cases
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int N = arr.length;
+            Solution ob = new Solution();
+            ob.nextPermutation(arr); // Generate the next permutation
+            StringBuilder out = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                out.append(arr[i] + " "); // Corrected: use arr[i] instead of arr.get(i)
+            }
+            System.out.println(out.toString().trim()); // Print the output
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
