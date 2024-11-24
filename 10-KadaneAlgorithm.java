611c492180854ea945e976912ea2804ff107f6ad
Tanmay Jadhav <tanmayj4044@gmail.com>
2024-11-24 12:18:58 +0530

    Create 10-KadaneAlgorithm.java

---
+++ 10-KadaneAlgorithm.java
@@ -0,0 +1,23 @@
+/*Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+Example:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+*/
+
+class Solution {
+
+    int maxSubarraySum(int[] arr) {
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+}
